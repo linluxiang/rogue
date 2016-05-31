763ae6e4f6f05113d3ec14ceb867bbfb89ae6888
Lin Luxiang <linluxiang@gmail.com>
2016-06-01 00:44:50 +0200

    add managers

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameObject;
+
+	void Awake() {
+		if (GameManager.instance == null) {
+			Debug.LogFormat ("init gameobject");
+			Instantiate (gameObject);
+		}
+	}
+}
